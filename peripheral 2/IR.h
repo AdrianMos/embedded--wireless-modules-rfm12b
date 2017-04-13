94a84f66aade4e69d157ea67b408120650198863
Adrian Mos <adrian.raul.mos@gmail.com>
2017-04-13 17:38:50 +0300

    periph2 cleanup - code allignment

---
+++ peripheral 2/IR.h
@@ -0,0 +1,13 @@
+#include "local.h"
+
+#define IR_TRANSITION_LH      1
+#define IR_TRANSITION_HL      2
+#define IR_NO_TRANSITION_LOW  3
+#define IR_NO_TRANSITION_HIGH 4
+
+
+extern void IR_init ();
+extern   u8 IR_transition();
+
+
+
